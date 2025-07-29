7f71f3da4eb3b0814e95167814a91cf52a7d3b59
Yuan-Tang Chou <yuan-tang.chou@cern.ch>
2025-07-29 22:33:12 +0200

    Add examples

--- NeuralForecasting/example_submission/trivial_submission/model.py
+++ NeuralForecasting/example_submission/trivial_submission/model.py
@@ -1,9 +1,15 @@
 
 
 class Model:
-    def __init__(self):
+    def __init__(self, monkey_name='beignet'):
         # You could include a constructor to initialize your model here, but all calls will be made to the load method
-        self.clf = None
+        self.monkey_name = monkey_name
+        if self.monkey_name == 'beignet':
+            print('Beignet')
+        elif self.monkey_name == 'affi':
+            print('Affi')
+        else:
+            raise ValueError(f'No such a monkey: {self.monkey_name}')
 
     def predict(self, X):
         # This method should accept an input of any size (of the given input format) and return predictions appropriately
